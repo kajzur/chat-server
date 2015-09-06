60a608acea580880ef6fd4259403e7da739d9c2e
Mateusz Mazurek <kajzur@o2.pl>
2015-09-06 10:10:15 +0200

    very simple client added

--- sample-client/code.js
+++ sample-client/code.js
@@ -4,13 +4,27 @@ var id2 = (id == 1?2:1);
 var exampleSocket = new WebSocket("ws://localhost:3333?token="+id);
 exampleSocket.onmessage = function (event) {
 	var msg = JSON.parse(event.data);
-	console.log(msg.message)
+	if(msg[0] === undefined){	
+
+		var validMessage = JSON.parse(msg.message);
+		document.getElementById("tresc").innerText+=validMessage.text+"\n";
+		console.log(msg)
+	}
+	
 }
 exampleSocket.onopen = function(event){
-	exampleSocket.send(JSON.stringify({
-		text:"test. Wysyłam do "+id2,
-		dateTime: Date.now(),
-		from:id,
-		to:id2
-	}));
+	console.info("connected.")
+}
+
+document.getElementById("send").onclick = function(){
+	var result = prompt("Wiadomosc:");
+	var to = prompt("Do kogo:");
+	if(result && to){
+		exampleSocket.send(JSON.stringify({
+			text:result,
+			dateTime: Date.now(),
+			from:id,
+			to:to
+		}));
+	}
 }
